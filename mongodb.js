135489dc8a7aa0236eb5bc276df7472cba0451fe
blakeroy01 <44304480+blakeroy01@users.noreply.github.com>
2020-05-11 23:11:53 -0400

    Start of app

---
+++ mongodb.js
@@ -0,0 +1,20 @@
+// CRUD create read update delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'Server-Project'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database')
+    }
+
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'Blake',
+        bio: 'my name is blake and i am 6 foot 7'
+    })
+})
